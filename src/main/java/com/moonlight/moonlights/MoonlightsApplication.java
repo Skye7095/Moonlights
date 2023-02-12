236ee2256daff00b5f469e1f6c097782c5da580e
Skye7095 <hkim7095@gmail.com>
2023-02-13 03:24:46 +0900

    - 최초 파일 추가     - jsp연동, database연동 등 기본세팅 및 서버작동여부 test

---
+++ src/main/java/com/moonlight/moonlights/MoonlightsApplication.java
@@ -0,0 +1,13 @@
+package com.moonlight.moonlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonlight/moonlights/MoonlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonlight.moonlights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
